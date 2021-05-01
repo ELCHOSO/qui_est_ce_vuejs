ce4ec781353e46813afb27b7d10d59bebdd74f97
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2021-05-01 16:57:57 +0200

    [ADD] character_file

---
+++ character_struct.js
@@ -0,0 +1,232 @@
+personnages = [
+    {
+        name = "alain",
+        hair_colour = 1,
+        hair_type = 0,
+        beard = 2,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/alain.jpg"
+    },
+    {
+        name = "ali",
+        hair_colour = 2,
+        hair_type = 1,
+        beard = 0,
+        glasses = 1,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/ali.jpg"
+    },
+    {
+        name = "amir",
+        hair_colour = 0,
+        hair_type = 0,
+        beard = 0,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/amir.jpg"
+    },
+    {
+        name = "christine",
+        hair_colour = 0,
+        hair_type = 10,
+        beard = 0,
+        glasses = 0,
+        gender = 1,
+        chapeau = 0,
+        sprite = "sprites/christine.jpg"
+    },
+    {
+        name = "diego",
+        hair_colour = 0,
+        hair_type = 1,
+        beard = 1,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/diego.jpg"
+    },
+    {
+        name = "hugo",
+        hair_colour = 3,
+        hair_type = 0,
+        beard = 3,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/hugo.jpg"
+    },
+    {
+        name = "ines",
+        hair_colour = 4,
+        hair_type = 10,
+        beard = 0,
+        glasses = 0,
+        gender = 1,
+        chapeau = 1,
+        sprite = "sprites/ines.jpg"
+    },
+    {
+        name = "jacques",
+        hair_colour = 0,
+        hair_type = 1,
+        beard = 3,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/jacques.jpg"
+    },
+    {
+        name = "julien",
+        hair_colour = 2,
+        hair_type = 1,
+        beard = 0,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/julien.jpg"
+    },
+    {
+        name = "laetitia",
+        hair_colour = 3,
+        hair_type = 10,
+        beard = 0,
+        glasses = 1,
+        gender = 1,
+        chapeau = 1,
+        sprite = "sprites/laetitia.jpg"
+    },
+    {
+        name = "leo",
+        hair_colour = 1,
+        hair_type = 0,
+        beard = 0,
+        glasses = 1,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/leo.jpg"
+    },
+    {
+        name = "liam",
+        hair_colour = 1,
+        hair_type = 10,
+        beard = 1,
+        glasses = 0,
+        gender = 0,
+        chapeau = 1,
+        sprite = "sprites/liam.jpg"
+    },
+    {
+        name = "lilou",
+        hair_colour = 4,
+        hair_type = 10,
+        beard = 0,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/lilou.jpg"
+    },
+    {
+        name = "mathieu",
+        hair_colour = 1,
+        hair_type = 100,
+        beard = 0,
+        glasses = 1,
+        gender = 0,
+        chapeau = 1,
+        sprite = "sprites/mathieu.jpg"
+    },
+    {
+        name = "mathis",
+        hair_colour = 4,
+        hair_type = 1,
+        beard = 3,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/mathis.jpg"
+    },
+    {
+        name = "nathan",
+        hair_colour = 3,
+        hair_type = 1,
+        beard = 0,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/nathan.jpg"
+    },
+    {
+        name = "noemie",
+        hair_colour = 1,
+        hair_type = 10,
+        beard = 0,
+        glasses = 0,
+        gender = 1,
+        chapeau = 0,
+        sprite = "sprites/noemie.jpg"
+    },
+    {
+        name = "pauline",
+        hair_colour = 1,
+        hair_type = 10,
+        beard = 0,
+        glasses = 1,
+        gender = 1,
+        chapeau = 0,
+        sprite = "sprites/pauline.jpg"
+    },
+    {
+        name = "sebastien",
+        hair_colour = 4,
+        hair_type = 100,
+        beard = 0,
+        glasses = 0,
+        gender = 0,
+        chapeau = 1,
+        sprite = "sprites/sebastien.jpg"
+    },
+    {
+        name = "sophie",
+        hair_colour = 3,
+        hair_type = 10,
+        beard = 0,
+        glasses = 1,
+        gender = 1,
+        chapeau = 0,
+        sprite = "sprites/sophie.jpg"
+    },
+    {
+        name = "stephane",
+        hair_colour = 4,
+        hair_type = 0,
+        beard = 1,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/stephane.jpg"
+    },
+    {
+        name = "thomas",
+        hair_colour = 2,
+        hair_type = 1,
+        beard = 3,
+        glasses = 0,
+        gender = 0,
+        chapeau = 0,
+        sprite = "sprites/thomas.jpg"
+    },
+    {
+        name = "banana",
+        hair_colour = -1,
+        hair_type = -1,
+        beard = -1,
+        glasses = -1,
+        gender = -1,
+        chapeau = -1,
+        sprite = "sprites/banana.jpg"
+    }
+]
